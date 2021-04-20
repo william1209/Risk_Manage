5fe49cb16281ea772b0f997d7eeb6012f453b034
william1209 <pro.imoney@gmail.com>
2021-04-20 15:30:03 +0800

    v1.0.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
           'pandas',
           'datetime',
           'numpy',
-          'pandas_datareader'
+          'pandas_datareader',
       ],
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
