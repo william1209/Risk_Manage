f6b5e896c567e228705caf890dc3f8d27442c4ac
william1209 <pro.imoney@gmail.com>
2021-04-20 15:05:45 +0800

    v1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'risk_manage',         # How you named your package folder (MyLib)
   packages = ['risk_manage'],   # Chose the same as "name"
-  version = '1.0',      # Start with a small number and increase it with every change you make
+  version = '1.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'test version ',   # Give a short description about your library
   author = 'Hao-Chen, Chiu',                   # Type in your name
