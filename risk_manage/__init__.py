73f6058799d0d683ac4d33b34d6aede08967eb7e
william1209 <pro.imoney@gmail.com>
2021-04-21 14:57:21 +0800

    Add New Class

--- risk_manage/__init__.py
+++ risk_manage/__init__.py
@@ -1,1 +1,2 @@
 from risk_manage.Data_Prepare import Data_Prepare
+from risk_manage.model_data_parse import model_data_parse
---
+++ risk_manage/model_data_parse.py
@@ -0,0 +1,34 @@
+import numpy as np
+class model_data_parse(Data_Prepare):
+  def __init__(self, target, daypara):
+    self.target = target
+    self.daypara = daypara
+    self.df = super(model_data_parse, self).fetch(target) 
+    self.diff = super(model_data_parse, self).diff_data(1)
+    self.df_np = np.array(self.df["price"]).reshape(-1,1)
+    #self.n = np.arange(1,8)
+
+
+  def parse_fig1(self, labels, n_class):
+    """
+    Parse Cluster Label Index V2
+    """
+    index = [[] for i in range(n_class)]
+    for k in range(n_class):
+      temp0 = list(filter(lambda i: i[1]==k, enumerate(labels)))
+      index[k] = [temp[0] for temp in temp0]
+    """
+    Parse Crash Index V2
+    """
+    temp = list(filter(lambda i: (abs(i[1])>300), enumerate(self.diff)))
+    crash_date_index = [i[0] for i in temp]
+
+    """
+    Parse Exact Crash Date
+    """
+    crash_date_sort = [[] for _ in range(n_class)]
+    for outer_i in range(n_class):
+      for i in range(len(index[outer_i])):
+        if self.df.index[index[outer_i]][i] in self.diff.index[crash_date_index]:
+          crash_date_sort[outer_i].append(self.df.index[index[outer_i]][i])
+    return index, crash_date_index, crash_date_sort
